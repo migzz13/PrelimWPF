aed3187a9ed518b34a95fd7da9884142092d562d
LUIS MIGUEL EMMANUEL MATIAS <miguelmatias3003@gmail.com>
2024-03-06 17:41:40 +0800

    Add project files.

---
+++ PrelimWPF/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrelimWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RuleBtn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Once the game starts, the program will generate a random 8-bit decimal number and start the 30-second timer." +
+                "Your goal is to convert the decimal number into it's binary form. The game will continue to generate new numbers for you " +
+                "to convert until the timer runs out.");
+        }
+
+        private void UserInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
